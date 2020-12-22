4fa726e4d60ab6d976de95d60b33a84e5e439911
Crowdbotics <team@crowdbotics.com>
2020-12-22 07:41:21 +0000

    Update components

--- src/features/CopyOfBlankScreen116077/screens/blank.js
+++ src/features/CopyOfBlankScreen116077/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,55 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = { Switch_3: true, Switch_4: false, DateTimePicker_5: new Date("") }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <Button
+        title="Press me!"
+        onPress={() => this.props.navigation.navigate("BlankScreen016065")}
+      />
+      <Switch
+        trackColor={{ false: "#d92641", true: "#dce736" }}
+        style={styles.Switch_3}
+        value={this.state.Switch_3}
+        onValueChange={nextChecked => this.setState({ Switch_3: nextChecked })}
+      />
+      <Switch
+        trackColor={{ false: "#d92641", true: "#dce736" }}
+        style={styles.Switch_4}
+        value={this.state.Switch_4}
+        onValueChange={nextChecked => this.setState({ Switch_4: nextChecked })}
+      />
+      <DateTimePicker
+        showIcon={false}
+        date={this.state.DateTimePicker_5}
+        onDateChange={selectedDate =>
+          this.setState({ DateTimePicker_5: selectedDate })
+        }
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-});
+  View_1: {},
+  Button_2: {},
+  Switch_3: { alignSelf: "flex-start" },
+  Switch_4: { alignSelf: "flex-start" },
+  DateTimePicker_5: {}
+})
