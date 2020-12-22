964566fe9d253e96d4f02727141bdb2a96e2ae33
Crowdbotics <team@crowdbotics.com>
2020-12-22 07:30:06 +0000

    Update components

--- src/features/CopyOfCopyOfBlankScreen216075/screens/blank.js
+++ src/features/CopyOfCopyOfBlankScreen216075/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -10,32 +10,59 @@ import {
   TextInput,
   StyleSheet,
   ScrollView
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {
+    TextInput_2: "hgfhfgfhgfhfhgfh ghfhfj",
+    TextInput_4: "6565",
+    TextInput_5:
+      "hgfhgfghf ghfghfgh hgfghfgh hgfghfg ghfhgfhf hgfghfghf ghfhgf hgfhgfgh hgfghf hgfhgf"
+  }
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <TextInput
+        placeholder="Sample text input placeholder"
+        value={this.state.TextInput_2}
+        onChangeText={nextValue => this.setState({ TextInput_2: nextValue })}
+      />
+      <Text>Sample text content ghfhgfhgfgh hgfghf</Text>
+      <TextInput
+        placeholder="Number Input Placeholder"
+        keyboardType="numeric"
+        value={this.state.TextInput_4}
+        onChangeText={nextValue => this.setState({ TextInput_4: nextValue })}
+      />
+      <TextInput
+        placeholder="Sample text input placeholder"
+        multiline={true}
+        value={this.state.TextInput_5}
+        onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
+      />
+      <Text>Sample text content</Text>
+      <Button title="Press me!" onPress={() => alert("Pressed!")} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginHorizontal: 16,
-  },
-});
+  View_1: {},
+  TextInput_2: {},
+  Text_3: {},
+  TextInput_4: {},
+  TextInput_5: {},
+  Text_6: {},
+  Button_7: {}
+})
